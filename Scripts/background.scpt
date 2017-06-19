ff81a05cd472c4de580b3fcaf027bf4f686dff42
Nick Gottschlich <nickpgott@gmail.com>
2017-06-18 22:31:33 -0500

    lots of clean up

--- Scripts/background.scpt
+++ Scripts/background.scpt
@@ -1,5 +1,5 @@
 tell application "iTerm"
 	tell current session of current window
-		set background image to "/Users/nick.gottschlich/Code/Personal/Pokemon-Terminal/./Images/Main/downloadedPic.jpg"
+		set background image to "/Users/nick.gottschlich/Code/Personal/Pokemon-Terminal/./Images/downloadedPic.jpg"
 	end tell
 end tell
--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 # The main module that brings everything together.
 
 from sys import argv
-from database import Database
 import scripter
 import sys
 import time
@@ -12,57 +11,12 @@ import random
 import requests
 import os
 
+
 reddit = praw.Reddit(
     client_id='BTwqpLqtP1t5qQ',
     client_secret='p0kQUlqO-1vyDZCy877B1M8nL4Q',
     user_agent='mac:iterm2-background:v0.0.1 (by /u/JavaOffScript)'
 )
-
-def print_list(list_of_items):
-    # Print all the items in a list. Used for printing each Anime from a particular region.
-    for item in list_of_items:
-        print(item)
-
-
-def print_columns(items):
-    # Print a list as multiple columns instead of just one.
-    rows = []
-    items_per_column = int(len(items) / 4) + 1
-
-    for index in range(0, len(items)):
-        anime = items[index]
-
-        if not anime.is_extra():
-            name = anime.get_id() + " " + str(anime.get_name()).capitalize()
-        else:
-            name = "--- " + anime.get_name()
-
-        name = name.ljust(20)
-
-        if len(rows) < items_per_column:
-            rows.append(name)
-        else:
-            rows[index % items_per_column] += name
-
-    print_list(rows)
-
-
-def prefix_search(db, arg):
-    # Find all Anime in database, db, with the prefix, arg.
-    result = db.names_with_prefix(arg)
-    if len(result) == 0:
-        print("No Anime found with prefix '" + arg + "'.")
-    else:
-        print_columns(result)
-
-
-def print_extra(db):
-    # Print all the 'Extra' Anime from the 'Extra' folder.
-    result = db.get_extra()
-    if len(result) == 0:
-        print("No Anime were found in Images/Extra.")
-    else:
-        print_columns(result)
 
 
 def print_usage():
@@ -70,50 +24,24 @@ def print_usage():
     print(
         '''
 Usage:
-    anime [parameter]
+    term-bg [parameter]
 
 Parameters:
-    [name]        -   Change the terminal background to the specified Anime.
-    [index]       -   Change the terminal background to a Anime by its index.
-    [region]      -   List all the Anime of the specified region.
-    [one letter]  -   List all Anime who's names begin with a particular letter.
-    [two letters] -   List all Anime who's names begin with those two letters.
-
-Other Parameters:
-    anime all             -   List all the Anime supported.
-    anime extra           -   List all the Anime from the 'Extra' folder.
-    anime random          -   Change the terminal background to a random Anime.
-    anime ?               -   Identify the current Anime in the terminal.
-    anime _pikachu        -   Change the wallpaper to the specified Anime.
-    anime _random         -   Change the wallpaper to a random Anime.
-    anime _?              -   Identify the current Anime in the wallpaper.
-    anime slideshow       -   Iterate through each Anime.
-    anime help            -   Display this menu.
+    [subreddit]   -   Grab a random image from 'subreddit', and make it your terminal background.
 ''')
-
-
-def slideshow(db, start, end):
-    # Show each Anime in order, one by one.
-    try:
-        for x in range(start, end):
-            anime = db.get_anime(x)
-            scripter.change_terminal(anime)
-            time.sleep(0.25)
-    except KeyboardInterrupt:
-        print("Program was terminated.")
-        sys.exit()
 
 
 def downloadImage(imageUrl, localFileName):
     response = requests.get(imageUrl)
     if response.status_code == 200:
         print('Downloading %s...' % (localFileName))
-        with open(os.path.join(os.getcwd()+'/Images/Main', localFileName), 'wb') as fo:
+        with open(os.path.join(os.getcwd()+'/Images', localFileName), 'wb') as fo:
             for chunk in response.iter_content(4096):
                 fo.write(chunk)
 
 
-def change_terminal_background(db, arg):
+def change_terminal_background(arg):
+    #next step here is to just check submission.url to see if it ends in jpg, then check if it's a common image upload site like imgur
     for submission in reddit.subreddit(arg).hot(limit=3):
         print(submission.url)
         if ('imgur.com' in submission.url):
@@ -134,88 +62,22 @@ def change_terminal_background(db, arg):
             print('not imgur')
         # print('imgur' in submission.url)
         # print('redd' in submission.url)
-        #
-
-
-    #okay so this succesfully goes to a subreddit
-    # for submission in reddit.subreddit(arg).top('week', limit=10):
-    # for submission in reddit.subreddit(arg).hot(limit=10):
-        # print(submission.title)
-
-    # submissionA = reddit.subreddit(arg).random()
-    # print(reddit.subreddit(arg).random())
-    # print(submissionA)
-    # print(submissionA.title)
-    # print(submissionA.url)
-
-    # well my idea is to generate a random number 1-100, get the hot 100 from a the inputted sub
-    # find that submission and grab the image, if not succesful, generate another number and try again? I guess?
-
-    # randnum = random.seed(time.time())
-
-    # print(randnum)
-
-
-
-def change_wallpaper(db, arg):
-    # Change the wallpaper to the specified Anime, if it exists.
-    if arg in db:
-        anime = db.get_anime(arg)
-        scripter.change_wallpaper(anime)
-    else:  # If not found in the database, try to give suggestions.
-        suggestions = db.names_with_infix(arg)
-        if len(suggestions) == 0:
-            print("No such Anime was found and no suggestions are available.")
-        elif len(suggestions) == 1:
-            scripter.change_wallpaper(suggestions[0])
-            print("Did you mean " + suggestions[0].get_name().capitalize() + "?")
-            scripter.change_wallpaper(suggestions[0])
-        else:
-            print("Result: " + arg)
-            print("Did you mean " + suggestions[0].get_name().capitalize() + "?")
-            print("Other suggestions:")
-            print_columns(suggestions[1:])
-            scripter.change_wallpaper(suggestions[0])
 
 
 def single_argument_handler(arg):
     # Handle the logic for when there is only one command line parameter inputted.
-    db = Database()
 
-    # If there is an escape code, then change the wallpaper, not the terminal.
-    if str(arg).startswith("_"):
-        escape_code = True
-        arg = arg[1:]
-    else:
-        escape_code = False
-
-    if len(arg) < 3 and arg.isalpha():
-        prefix_search(db, arg)
-    elif arg == "extra":
-        print_extra(db)
-    elif arg == "help":
+    if arg == "help":
         print_usage()
-    elif arg == "random" and escape_code:
-        change_wallpaper(db, db.get_random().get_name())
-    elif arg == "random":
-        change_terminal_background(db, db.get_random().get_name())
-    elif arg == "slideshow":
-        slideshow(db, 1, 494)
-    elif arg == "?" and escape_code:
-        scripter.determine_wallpaper_anime(db)
-    elif arg == "?":
-        scripter.determine_terminal_anime(db)
-    elif escape_code:
-        change_wallpaper(db, arg)
     else:
-        change_terminal_background(db, arg)
+        change_terminal_background(arg)
 
 
 if __name__ == "__main__":
     # Entrance to the program.
     if len(argv) == 1:
         print("No command line arguments specified."
-              "\nTry typing in a Anime name or number."
+              "\nTry inputting in a subreddit with image posts."
               "\nOr type \"help\" to see all the commands.")
     elif len(argv) == 2:
         single_argument_handler(argv[1].lower())
